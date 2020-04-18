ff794bb3f7bb15fdf1038ee63a00dddaf122f3f1
杨景 <32809255+YangJing524@users.noreply.github.com>
2020-04-18 23:51:07 +0800

    20200418
    au-yj

--- 工具与代码/gis_toolboxPy_beijingPoi.py
+++ 工具与代码/gis_toolboxPy_beijingPoi.py
@@ -29,206 +29,206 @@ out_Layer=os.path.join(output_tabel_path,categor_param)
 saved_Layer = os.path.split(output_tabel_path)[0]+'\\'+ categor_param+ '_' +city_param + '.lyr'
 
 if not arcpy.Exists(output_tabel_path):
-    arcpy.AddMessage("the gdb file dose not exists")
-    sys.exit(0)
+	arcpy.AddMessage("the gdb file dose not exists")
+	sys.exit(0)
 
 header={
-        "Authorization": "",
-        "X-Auth-Token": "",
-        "User-Agent": "com.tianyancha.skyeye/Dalvik/2.1.0 (Linux; U; Android 9; oppo qbs Build/PKQ1.180819.001;)",
-        "version": "Android 8.5.1",
-        "deviceID": "",
-        "channelID": "PPZhuShou",
-        "Content-Type": "application/json",
-        "Host": "restapi.amap.com",
-        "Connection": "Keep-Alive",
-        "Accept-Encoding": "gzip",
-    }
+		"Authorization": "",
+		"X-Auth-Token": "",
+		"User-Agent": "com.tianyancha.skyeye/Dalvik/2.1.0 (Linux; U; Android 9; oppo qbs Build/PKQ1.180819.001;)",
+		"version": "Android 8.5.1",
+		"deviceID": "",
+		"channelID": "PPZhuShou",
+		"Content-Type": "application/json",
+		"Host": "restapi.amap.com",
+		"Connection": "Keep-Alive",
+		"Accept-Encoding": "gzip",
+	}
 
 def gcj2wgs(loc):
 
-    lon = float(loc.split(',')[0])
-    lat = float(loc.split(',')[1])
-    a = 6378245.0
-    ee = 0.00669342162296594323
-    PI = 3.14159265358979324
-    # convert way
-    x = lon - 105.0
-    y = lat - 35.0
-    # longtitude
-    dLon = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * math.sqrt(abs(x))
-    dLon += (20.0 * math.sin(6.0 * x * PI) + 20.0 * math.sin(2.0 * x * PI)) * 2.0 / 3.0
-    dLon += (20.0 * math.sin(x * PI) + 40.0 * math.sin(x / 3.0 * PI)) * 2.0 / 3.0
-    dLon += (150.0 * math.sin(x / 12.0 * PI) + 300.0 * math.sin(x / 30.0 * PI)) * 2.0 / 3.0
-    # lattitude
-    dLat = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * math.sqrt(abs(x))
-    dLat += (20.0 * math.sin(6.0 * x * PI) + 20.0 * math.sin(2.0 * x * PI)) * 2.0 / 3.0
-    dLat += (20.0 * math.sin(y * PI) + 40.0 * math.sin(y / 3.0 * PI)) * 2.0 / 3.0
-    dLat += (160.0 * math.sin(y / 12.0 * PI) + 320 * math.sin(y * PI / 30.0)) * 2.0 / 3.0
-    radLat = lat / 180.0 * PI
-    magic = math.sin(radLat)
-    magic = 1 - ee * magic * magic
-    sqrtMagic = math.sqrt(magic)
-    dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * PI);
-    dLon = (dLon * 180.0) / (a / sqrtMagic * math.cos(radLat) * PI);
-    wgsLon = lon - dLon
-    wgsLat = lat - dLat
-    return wgsLon, wgsLat
+	lon = float(loc.split(',')[0])
+	lat = float(loc.split(',')[1])
+	a = 6378245.0
+	ee = 0.00669342162296594323
+	PI = 3.14159265358979324
+	# convert way
+	x = lon - 105.0
+	y = lat - 35.0
+	# longtitude
+	dLon = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * math.sqrt(abs(x))
+	dLon += (20.0 * math.sin(6.0 * x * PI) + 20.0 * math.sin(2.0 * x * PI)) * 2.0 / 3.0
+	dLon += (20.0 * math.sin(x * PI) + 40.0 * math.sin(x / 3.0 * PI)) * 2.0 / 3.0
+	dLon += (150.0 * math.sin(x / 12.0 * PI) + 300.0 * math.sin(x / 30.0 * PI)) * 2.0 / 3.0
+	# lattitude
+	dLat = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * math.sqrt(abs(x))
+	dLat += (20.0 * math.sin(6.0 * x * PI) + 20.0 * math.sin(2.0 * x * PI)) * 2.0 / 3.0
+	dLat += (20.0 * math.sin(y * PI) + 40.0 * math.sin(y / 3.0 * PI)) * 2.0 / 3.0
+	dLat += (160.0 * math.sin(y / 12.0 * PI) + 320 * math.sin(y * PI / 30.0)) * 2.0 / 3.0
+	radLat = lat / 180.0 * PI
+	magic = math.sin(radLat)
+	magic = 1 - ee * magic * magic
+	sqrtMagic = math.sqrt(magic)
+	dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * PI);
+	dLon = (dLon * 180.0) / (a / sqrtMagic * math.cos(radLat) * PI);
+	wgsLon = lon - dLon
+	wgsLat = lat - dLat
+	return wgsLon, wgsLat
 
 def geocodeBaidu(address):
-    address=re.sub('%s|中国%s'%(province_param,province_param),'',address)
-    if len(re.findall('%s'%city_param,address))==2:
-        address=re.sub('%s'%city_param,'',address,count=1)
-    if len(address)>=17:
-        address=address[: 17]+'...'
-    base_url = "http://api.map.baidu.com/geocoder?address={address}&output=json&key=yXr0pGqmWbFMEapTD2bxg2D362dCVinl".format(address=address)
+	address=re.sub('%s|中国%s'%(province_param,province_param),'',address)
+	if len(re.findall('%s'%city_param,address))==2:
+		address=re.sub('%s'%city_param,'',address,count=1)
+	if len(address)>=17:
+		address=address[: 17]+'...'
+	base_url = "http://api.map.baidu.com/geocoder?address={address}&output=json&key=yXr0pGqmWbFMEapTD2bxg2D362dCVinl".format(address=address)
 
-    response = requests.get(base_url)
+	response = requests.get(base_url)
 
-    try:
-        answer = response.json()
-        latitude = answer['result']['location']['lat']
-        longitude = answer['result']['location']['lng']
-        # time.sleep(1)
-        return longitude,latitude
-    except :
-        arcpy.AddMessage('No response in BaiduMap :%s'%response.status_code)
-        return '',''
+	try:
+		answer = response.json()
+		latitude = answer['result']['location']['lat']
+		longitude = answer['result']['location']['lng']
+		# time.sleep(1)
+		return longitude,latitude
+	except :
+		arcpy.AddMessage('No response in BaiduMap :%s'%response.status_code)
+		return '',''
 
 
 def geocodeGaode(address):
-    key='b01803a8d6382fc3dbeaf2d7fcf319df'
-    base_url = "https://restapi.amap.com/v3/place/text?keywords={}&city={}&offset=20&page=1&key={}&extensions=base".format(address,city_param,key)
-    response = requests.get(base_url,headers=header)
-    try:
-        answer = response.json()
-        # locat = answer['pois'][0]['location']
-        # latitude=locat.split(',')[1]
-        # longitude = locat.split(',')[0]
-        # # tel=answer['pois'][0]['tel']
-        # # time.sleep(1)s
-        # return longitude, latitude
+	key='b01803a8d6382fc3dbeaf2d7fcf319df'
+	base_url = "https://restapi.amap.com/v3/place/text?keywords={}&city={}&offset=20&page=1&key={}&extensions=base".format(address,city_param,key)
+	response = requests.get(base_url,headers=header)
+	try:
+		answer = response.json()
+		# locat = answer['pois'][0]['location']
+		# latitude=locat.split(',')[1]
+		# longitude = locat.split(',')[0]
+		# # tel=answer['pois'][0]['tel']
+		# # time.sleep(1)s
+		# return longitude, latitude
 
-        return answer['pois'][0]
-    except:
-        arcpy.AddMessage(response.status_code)
-        return '', ''
+		return answer['pois'][0]
+	except:
+		arcpy.AddMessage(response.status_code)
+		return '', ''
 
 
 def getGaodePois(keyword):
 
-    key = 'b01803a8d6382fc3dbeaf2d7fcf319df'
-    # Eight locations
-    #city_code=["110101","110102","110105","110106","110107","110108"]
-    city_code=["110102"]
+	key = 'b01803a8d6382fc3dbeaf2d7fcf319df'
+	# Eight locations
+	#city_code=["110101","110102","110105","110106","110107","110108"]
+	city_code=["110102"]
 
 
-    url1 = "https://restapi.amap.com/v3/place/text?keywords="+keyword+"&city="
-    url2 = "&output=JSON&offset=45&key=b01803a8d6382fc3dbeaf2d7fcf319df&extensions=all&page="
-    x = []
-    col=['name','type','address','adname','long','lat']
-    num = 0
-    for i in range(0, len(city_code)):
-        city = city_code[i]
+	url1 = "https://restapi.amap.com/v3/place/text?keywords="+keyword+"&city="
+	url2 = "&output=JSON&offset=45&key=b01803a8d6382fc3dbeaf2d7fcf319df&extensions=all&page="
+	x = []
+	col=['name','type','address','adname','long','lat']
+	num = 0
+	for i in range(0, len(city_code)):
+		city = city_code[i]
 		# set the max deep of page is 80  optional:1~100
-        for page in range(1, 80):
-            # if page == 45:
-            #     pass
-            thisUrl = url1 + city + url2 + str(page)
-            data = requests.get(thisUrl)
-            s = data.json()
-            aa = s["pois"]
-            if len(aa) == 0:
-                break
-            for k in range(0, len(aa)):
-                s1 = aa[k]["name"]
-                s2 = aa[k]["type"]
-                s3 = aa[k]["address"]
-                # to avoid the ValueError: setting an array element with a sequence.
-                if isinstance(s3,list):
-                    s3=' '
-                s4 = aa[k]["adname"]
-                long,lat = gcj2wgs(aa[k]["location"])
-                # to avoid ValueError: setting an array element with a sequence
-                x.append([s1, s2, s3, s4, float(long), float(lat)])
+		for page in range(1, 80):
+			# if page == 45:
+			#     pass
+			thisUrl = url1 + city + url2 + str(page)
+			data = requests.get(thisUrl)
+			s = data.json()
+			aa = s["pois"]
+			if len(aa) == 0:
+				break
+			for k in range(0, len(aa)):
+				s1 = aa[k]["name"]
+				s2 = aa[k]["type"]
+				s3 = aa[k]["address"]
+				# to avoid the ValueError: setting an array element with a sequence.
+				if isinstance(s3,list):
+					s3=' '
+				s4 = aa[k]["adname"]
+				long,lat = gcj2wgs(aa[k]["location"])
+				# to avoid ValueError: setting an array element with a sequence
+				x.append([s1, s2, s3, s4, float(long), float(lat)])
 
-                num += 1
-                arcpy.AddMessage("Crawl " + str(num) + " pieces of data")
-            time.sleep(1)
+				num += 1
+				arcpy.AddMessage("Crawl " + str(num) + " pieces of data")
+			time.sleep(1)
 
-    arcpy.AddMessage("==Success crawl data from internet==")
-    arcpy.AddMessage("=======Made by YangJing==========")
-    # c = pd.DataFrame(x) #unable to install pandas
-    # try:
-    #     c.to_excel(keyword+r'.xlsx', index = False,columns=col)
-    # except:
-    #     c.to_csv(keyword+'.csv', encoding='utf-8-sig',index = False,columns=col)
+	arcpy.AddMessage("==Success crawl data from internet==")
+	arcpy.AddMessage("=======Made by YangJing==========")
+	# c = pd.DataFrame(x) #unable to install pandas
+	# try:
+	#     c.to_excel(keyword+r'.xlsx', index = False,columns=col)
+	# except:
+	#     c.to_csv(keyword+'.csv', encoding='utf-8-sig',index = False,columns=col)
 
-    arr=np.array(x)
-    # np.save('01.npy',arr)
-    arcpy.AddMessage("...Store data to csv...")
-    #
-    struct_array = np.core.records.fromarrays(arr.transpose(), np.dtype(
-        [('name', 'U50'), ('type', 'U50'), ('address', 'U150'), ('adname', 'U50'), ('long', '<f8'), ('lat', '<f8')]))
-    try:
-        arcpy.da.NumPyArrayToTable(struct_array, output_tabel_path+'\\'+categor_param+ '_' +city_param)
-    except:
-        arcpy.Delete_management(output_tabel_path+'\\'+categor_param+ '_' +city_param)
-        arcpy.da.NumPyArrayToTable(struct_array, output_tabel_path + '\\' + categor_param + '_' +city_param)
+	arr=np.array(x)
+	# np.save('01.npy',arr)
+	arcpy.AddMessage("...Store data to csv...")
+	#
+	struct_array = np.core.records.fromarrays(arr.transpose(), np.dtype(
+		[('name', 'U50'), ('type', 'U50'), ('address', 'U150'), ('adname', 'U50'), ('long', '<f8'), ('lat', '<f8')]))
+	try:
+		arcpy.da.NumPyArrayToTable(struct_array, output_tabel_path+'\\'+categor_param+ '_' +city_param)
+	except:
+		arcpy.Delete_management(output_tabel_path+'\\'+categor_param+ '_' +city_param)
+		arcpy.da.NumPyArrayToTable(struct_array, output_tabel_path + '\\' + categor_param + '_' +city_param)
 
 
-    arcpy.AddMessage("The data has stored in "+output_tabel_path+'\\'+categor_param)
-    arcpy.AddMessage("==================================")
+	arcpy.AddMessage("The data has stored in "+output_tabel_path+'\\'+categor_param)
+	arcpy.AddMessage("==================================")
 
-    arcpy.AddMessage("Display xy data")
-    spRef=arcpy.SpatialReference("WGS 1984")
+	arcpy.AddMessage("Display xy data")
+	spRef=arcpy.SpatialReference("WGS 1984")
 	# xy transform to point
-    arcpy.MakeXYEventLayer_management(output_tabel_path+'\\'+categor_param+ '_' +city_param, "long", "lat", out_Layer, spRef)
-    arcpy.SaveToLayerFile_management(out_Layer, saved_Layer)
+	arcpy.MakeXYEventLayer_management(output_tabel_path+'\\'+categor_param+ '_' +city_param, "long", "lat", out_Layer, spRef)
+	arcpy.SaveToLayerFile_management(out_Layer, saved_Layer)
 
 
 def getShapefile():
-    key = 'b01803a8d6382fc3dbeaf2d7fcf319df'
-    # subdistrict=1 can set [0,1,2,3]
-    url = "http://restapi.amap.com/v3/config/district?extensions=all&subdistrict=0&key=" + key
+	key = 'b01803a8d6382fc3dbeaf2d7fcf319df'
+	# subdistrict=1 can set [0,1,2,3]
+	url = "http://restapi.amap.com/v3/config/district?extensions=all&subdistrict=0&key=" + key
 
-    city_code = ["110100","110101", "110102", "110105", "110106", "110107", "110108"]
-    def get_line():
-        spRef = arcpy.SpatialReference("WGS 1984")
-        name=districts_level['name']
-        lines = districts_level['polyline'].split('|')
-        featureList=[]
-        for line in lines:
-            points = line.split(";")
-            point = arcpy.Point()
-            array = arcpy.Array()
-            for p in points:
-                point.X, point.Y = gcj2wgs(p)
-                array.add(point)
-            polyline = arcpy.Polygon(array, spRef)
-            featureList.append(polyline)
+	city_code = ["110100","110101", "110102", "110105", "110106", "110107", "110108"]
+	def get_line():
+		spRef = arcpy.SpatialReference("WGS 1984")
+		name=districts_level['name']
+		lines = districts_level['polyline'].split('|')
+		featureList=[]
+		for line in lines:
+			points = line.split(";")
+			point = arcpy.Point()
+			array = arcpy.Array()
+			for p in points:
+				point.X, point.Y = gcj2wgs(p)
+				array.add(point)
+			polyline = arcpy.Polygon(array, spRef)
+			featureList.append(polyline)
 
-        arcpy.CopyFeatures_management(featureList, os.path.split(output_tabel_path)[0]+"\\"+city_param+'_'+name+'.shp')
+		arcpy.CopyFeatures_management(featureList, os.path.split(output_tabel_path)[0]+"\\"+city_param+'_'+name+'.shp')
 
-    # http://restapi.amap.com/v3/config/district?extensions=all&subdistrict=0&key=b01803a8d6382fc3dbeaf2d7fcf319df&keywords=110103
-    for adcode in city_code:
-        _url = url + "&keywords=" + adcode
-        response = requests.get(_url, headers=header)
-        try:
-            districts_level = response.json()['districts'][0]
-        except:
-            districts_level = response.json()
-        # districts_level = shapes['districts']
-        get_line()
+	# http://restapi.amap.com/v3/config/district?extensions=all&subdistrict=0&key=b01803a8d6382fc3dbeaf2d7fcf319df&keywords=110103
+	for adcode in city_code:
+		_url = url + "&keywords=" + adcode
+		response = requests.get(_url, headers=header)
+		try:
+			districts_level = response.json()['districts'][0]
+		except:
+			districts_level = response.json()
+		# districts_level = shapes['districts']
+		get_line()
 
-    arcpy.AddMessage("==Success to create shape file==")
-    arcpy.AddMessage("==== Made by YangJing =====")
-    time.sleep(3)
-    # try:
-    #     get_line()
-    # except :
-    #     arcpy.AddMessage("Can not create polyline" )
-    #     pass
+	arcpy.AddMessage("==Success to create shape file==")
+	arcpy.AddMessage("==== Made by YangJing =====")
+	time.sleep(3)
+	# try:
+	#     get_line()
+	# except :
+	#     arcpy.AddMessage("Can not create polyline" )
+	#     pass
 
 
 getGaodePois(categor_param)
--- 路网畅通情况数据库/路网畅阻.py
+++ 路网畅通情况数据库/路网畅阻.py
@@ -1,14 +1,23 @@
+# -*- coding: utf-8 -*-
 import json
 import pickle
+import io
 import demjson
 import requests
 import time
+import numpy as np
 import math
+import arcpy
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 def gcj2wgs(loc):
-
-    lon = float(loc.split(',')[0])
-    lat = float(loc.split(',')[1])
+    try:
+        lon = float(loc.split(',')[0])
+        lat = float(loc.split(',')[1])
+    except IndexError as e:
+        print loc
     a = 6378245.0
     ee = 0.00669342162296594323
     PI = 3.14159265358979324
@@ -36,87 +45,143 @@ def gcj2wgs(loc):
     return wgsLon, wgsLat
 
 
+def creatTable(roadData):
+    # with open('{}.txt'.format(file_name),'rb+') as f:
+    #     roadData = pickle.load(f)
 
-'''
-{'info': 'OK',
-'infocode': '10000',
-'status': '1',
-'trafficinfo': {'description': '西局东街：西三环南路附近双向畅通。',
-'evaluation': {'blocked': '0.00%',
-'congested': '0.00%',
-'description': '整体畅通',
-'expedite': '100.00%',
-'status': '1',
-'unknown': '0.00%'},
-'roads': [{'angle': '2',
-'direction': '从西三环南路辅路到北京西站南路',
-'lcodes': '7954,7955',
-'name': '西局东街',
-'polyline': '116.311913,39.8748627;116.313171,39.8748627;116.313408,39.8748703;116.315331,39.8748665;116.31601,39.8748703;116.316269,39.8748741;116.316376,39.8748894;116.316696,39.8749352;116.316833,39.8749542;116.31694,39.8749771;116.317284,39.8750648;116.31739,39.8750916;116.317467,39.8751183;116.317543,39.8751411;116.317627,39.8751564;116.31797,39.8751717;116.318634,39.8751717;116.319412,39.8751602;116.319771,39.8751717;116.319885,39.8751678;116.320038,39.8751717;116.320808,39.8751602;116.320961,39.8751602;116.321075,39.8751564',
-'speed': '20',
-'status': '1'},
-{'angle': '183',
-'direction': '从太平桥路到西三环南路辅路',
-'lcodes': '-7953',
-'name': '西局东街',
-'polyline': '116.317963,39.875206;116.317619,39.8751831;116.317535,39.8751717;116.317459,39.875145;116.317368,39.8751183;116.317284,39.8750916;116.316933,39.8750076;116.316818,39.8749886;116.316673,39.8749619;116.316376,39.8749161;116.316269,39.8749008;116.31601,39.874897;116.315331,39.874897;116.313408,39.874897;116.313171,39.8748932;116.311913,39.8748932',
-'speed': '20',
-'status': '1'}]}}
-'''
+    _length = len(roadData)
+    # featureList = []
+    DATA=[]
+    for i in range(_length):
+        dict_data=json.loads(roadData[i])
+        if dict_data['info'] == 'OK':
+            def get_line():
+                # Attribute
+
+                try:
+                    roadName = dict_data['trafficinfo']['roads'][0]['name']
+                    description = dict_data['trafficinfo']['description']
+                    block = dict_data['trafficinfo']['evaluation']['blocked']
+                    congested = dict_data['trafficinfo']['evaluation']['congested']
+
+                    expedite = dict_data['trafficinfo']['evaluation']['expedite']
+                    polyline = dict_data['trafficinfo']['roads'][0]['polyline']
+                    if roadName and description and polyline:DATA.append([roadName,description,block,congested,expedite,polyline])
+                except Exception as e:
+                    # arcpy.AddMessage(e)
+                    return None
+                # spRef = arcpy.SpatialReference("WGS 1984")
+                # for points in polyline.split(';'):
+                #     point = arcpy.Point()
+                #     array = arcpy.Array()
+                #     point.X, point.Y = gcj2wgs(points)
+                #     array.add(point)
+                #
+                # polyline = arcpy.Polyline(array, spRef)
+                # struct_array = np.core.records.fromarrays(arr.transpose(), np.dtype(
+                #     [('name', 'U50'), ('type', 'U50'), ('address', 'U150'), ('adname', 'U50'), ('long', '<f8'),
+                #      ('lat', '<f8')]))
+
+                # arcpy.AddField_management(polyline, "roadName", "TEXT")
+                # arcpy.AddField_management(polyline, "description", "TEXT")
+                # arcpy.AddField_management(polyline, "block", "TEXT")
+                # arcpy.AddField_management(polyline, "congested", "TEXT")
+                # arcpy.AddField_management(polyline, "expedite", "TEXT")
+                #
+                # arcpy.CalculateField_management(polyline, 'roadName', str(roadName), "PYTHON_9.3")
+                # arcpy.CalculateField_management(polyline, "description", str(description), "PYTHON_9.3")
+                # arcpy.CalculateField_management(polyline, "block", block, "PYTHON_9.3")
+                # arcpy.CalculateField_management(polyline, "congested", congested, "PYTHON_9.3")
+                # arcpy.CalculateField_management(polyline, "expedite", expedite, "PYTHON_9.3")
+                return polyline
+            get_line()
+            # if line: featureList.append(line)
+
+        else:
+            print('parse error')
+            pass
+    arr=np.array(DATA)
+    struct_array = np.core.records.fromarrays(arr.transpose(), np.dtype(
+        [('roadName', 'U50'), ('description', 'U150'), ('block', 'U15'), ('congested', 'U15'), ('expedite', 'U15'),('polyline', 'U150')]))
+
+    name=file_name.replace(' ','_')
+    try:
+        arcpy.da.NumPyArrayToTable(struct_array, gdb_file + '\\' + name)
+    except Exception as e:
+        arcpy.AddMessage(e)
 
 
+    # arcpy.CopyFeatures_management(featureList, file_name + '.shp')
 
-header={
-        "Authorization": "",
-        "X-Auth-Token": "",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
-        "version": "Android 8.5.1",
-        "deviceID": "",
-        "Content-Type": "application/json",
-        "Host": "lbs.amap.com",
-        "Connection": "Keep-Alive",
-        "Accept-Encoding": "gzip",
-    }
-
-
-key = ['27ff9958e48d17b03afa5cf06e9ae185',
-       '35dc4d625c8f5b84664b539f4c401dfe',
-       'da0d3354c56524855a593987ea0b92a8',
-       '703bad79f66eca19753ef0cf50ea27b3',
-       'b987a8e0b64066946ec2ecffd8058d6d',
-       '7b72177ac4bb0343cee4752393237215',
-       'acb3533c2ef86be19384965e269868a4',
-       '3118c58ea0560fe4052eb10c8c45a9eb',
-       '8a10b13049fdf288efd116fba637ec7a',
-       'b01803a8d6382fc3dbeaf2d7fcf319df'
-       ]
-
-data = json.load(open('西城区格网坐标点.json','r',encoding='utf8'))
-a = demjson.decode(data[0]['_jsonBounding'])
-url = "https://restapi.amap.com/v3/traffic/status/rectangle?rectangle={0},{1};{2},{3}&extensions=all&key="+key[0]
-d = {}
-currentTime = time.ctime()
-_length = len(a)
-for i in range(_length):
-    x_min=a[i]['x_min']
-    y_min=a[i]['y_min']
-    x_max=a[i]['x_max']
-    y_max=a[i]['y_max']
-    response = requests.get(url.format(x_min,y_min,x_max,y_max),headers=header)
-    d[i]=response.content.decode('utf-8')
-    d['Datetime']=currentTime
-    d['Weather']='多云 12~23C'
-    time.sleep(round(1/18,3))
-
-tmp=time.ctime()[:-5].split(':')
-
-file_name=tmp[0]+'时'+tmp[1]+'分'+tmp[2]+'秒'
-
-with open('{}.txt'.format(file_name),'wb') as f:
-    pickle.dump(d,f)
-
-# with open('dump.txt','rb+') as f:
-#     pickle.load(f)
+    arcpy.AddMessage("==Success to create ROAD shapeFile==")
+    arcpy.AddMessage("==== Made by YangJing =====")
 
 
 
+def getRawJsonFile(file_name):
+    header={
+            "Authorization": "",
+            "X-Auth-Token": "",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
+            "version": "Android 8.5.1",
+            "deviceID": "",
+            "Content-Type": "application/json",
+            "Host": "lbs.amap.com",
+            "Connection": "Keep-Alive",
+            "Accept-Encoding": "gzip",
+        }
+
+
+    key = ['27ff9958e48d17b03afa5cf06e9ae185',
+           '35dc4d625c8f5b84664b539f4c401dfe',
+           'da0d3354c56524855a593987ea0b92a8',
+           '703bad79f66eca19753ef0cf50ea27b3',
+           'b987a8e0b64066946ec2ecffd8058d6d',
+           '7b72177ac4bb0343cee4752393237215',
+           'acb3533c2ef86be19384965e269868a4',
+           '3118c58ea0560fe4052eb10c8c45a9eb',
+           '8a10b13049fdf288efd116fba637ec7a',
+           'b01803a8d6382fc3dbeaf2d7fcf319df'
+           ]
+    f='西城区格网坐标点.json'.decode("utf-8")
+    # right answer
+    data = json.load(io.open(f,'r',encoding='utf8'))
+    #
+    a = demjson.decode(data[0]['_jsonBounding'])
+    url = "https://restapi.amap.com/v3/traffic/status/rectangle?rectangle={0},{1};{2},{3}&extensions=all&key="+key[5]
+    d = {}
+    # currentTime = time.ctime()
+    _length = len(a)
+    for i in range(_length):
+        x_min=a[i]['x_min']
+        y_min=a[i]['y_min']
+        x_max=a[i]['x_max']
+        y_max=a[i]['y_max']
+        response = requests.get(url.format(x_min,y_min,x_max,y_max),headers=header)
+        d[i]=response.content.decode('utf-8')
+        time.sleep(round(1 / 18, 3))
+    # d['Datetime']=currentTime
+    # d['Weather']='多云 12~23C'
+    # creatline() function
+
+    with open(unicode('{}.txt'.format(file_name),'utf-8'),'wb') as f:
+        pickle.dump(d,f)
+    arcpy.AddMessage('Success to save data')
+
+
+
+
+
+if __name__=='__main__':
+    tmp = time.ctime()[:-5].split(':')
+    file_name = tmp[0] + '时' + tmp[1] + '分' + tmp[2] + '秒'
+    print (file_name)
+    # right-answer
+    # open(unicode('{}.txt'.format(file_name),'utf-8'), 'wb')
+    # getRawJsonFile(file_name)
+    # file_name='Sat Apr 18 18时53分15秒'
+    gdb_file=r'C:\Users\yj\Desktop\城 · 垣\路网畅通情况数据库\实时路网畅阻情况数据.gdb'
+    file_name='Sat Apr 18 23时03分41秒'
+    with open(unicode('{}.txt'.format(file_name),'utf-8'),'rb+') as f:
+        roadData = pickle.load(f)
+        creatTable(roadData)
